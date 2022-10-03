299ec401f278e344d886e0df30246ac4d9de2eaa
rlawndks4 <rlawndks0423@naver.com>
2022-10-03 22:52:11 +0900

    asdf

--- src/data/Data.js
+++ src/data/Data.js
@@ -2,11 +2,11 @@ export const ObjColor = {
     background1:'#1dbf73',
     font1:'#5a5a5a'
 }
-export const backUrl = 'https://mern-back-ten.vercel.app';
+export const backUrl = 'https://oversee-back.vercel.app';
 
-const cloud_name=`showping-mern`;
+const cloud_name=`dobiitrvx`;
 export const cloudinary = {
     url:`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
-    preset_name:`npdsrqvr`,
-    api_key:`899436928959928`
+    preset_name:`xfsdtao2`,
+    api_key:`528177166119895`
 };
